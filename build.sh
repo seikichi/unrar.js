7f77a0d738b507e208278f43ff21e9370bc9320e
seikichi <seikichi@kmc.gr.jp>
2013-11-02 11:25:42 +0900

    add -O2 option to the emcc compilation

--- build.sh
+++ build.sh
@@ -13,11 +13,24 @@ fi
 cd ..
 patch -p0 -E < unrar-free.patch
 
-# export EMCC_CFLAGS="-O2"
-
 emcc \
-    -s LINKABLE=1 \
-    -s ALLOW_MEMORY_GROWTH=1 \
+    -s TOTAL_MEMORY=167772160 \
+    -s TOTAL_STACK=52428800 \
+    -O2 \
+    -s ASM_JS=0 \
+    -s EXPORTED_FUNCTIONS="["\
+"'_urarlib_list',"\
+"'_urarlib_freelist',"\
+"'_urarlib_get',"\
+"'_get_item_from_archive_list',"\
+"'_get_next_from_archive_list',"\
+"'_get_name_from_archive_entry',"\
+"'_get_pack_size_from_archive_entry',"\
+"'_get_unp_size_from_archive_entry',"\
+"'_get_host_os_from_archive_entry',"\
+"'_get_file_time_from_archive_entry',"\
+"'_get_file_attr_from_archive_entry',"\
+"'FS']" \
     export.c \
     unrar/src/unpack29.c \
     unrar/src/unrar15.c \
@@ -28,6 +41,7 @@ emcc \
     unrar/src/unrarvm.c \
     unrar/src/unrar29.c \
     unrar/src/unrarlib.c \
+    -D UNPACK29_KEEP_TEMP_FILE\
     -I unrar/src \
     -o _unrar.js
 cat _unrar.js export.js > unrar.js
